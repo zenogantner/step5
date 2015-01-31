867053bd5025cd6bab4b60053752ff90a805ff19
Zeno Gantner <zeno.gantner@gmail.com>
2015-01-31 15:46:32 +0100

    introduce user averaging; get rid of specific types for users and items again

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # step5
 Some recommendation code in Scala
+https://github.com/zenogantner/step5
+
+Developed using Scala 2.9.2.
+
+Feedback via pull request on GitHub or by e-mail: zeno.gantner@gmail.com
--- Step5.scala
+++ Step5.scala
@@ -1,19 +1,41 @@
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.Map
 import scala.io.Source
 import scala.math
 
-class User(id: Int) {}
-class Item(id: Int) {}
-class Rating(val user: User, val item: Item, val value: Double) {}
+// TODO define user and item classes/types to achieve more safety ... but for this I need to first learn how to properly implement equality for user-defined types ...
+
+class Rating(val user: Int, val item: Int, val value: Double) {}
 
 
 trait Rater {
-  def rate(user: User, item: Item): Double
+  def rate(user: Int, item: Int): Double
 }
 
 
 class GlobalAverage(val average: Double) extends Rater {
   def this(ratings: Seq[Rating]) = this(ratings.map(_.value).sum / ratings.length)
-  def rate(user: User, item: Item) = average
+  def rate(user: Int, item: Int) = average
+}
+
+
+// TODO this must be more elegant
+object Util {
+  def userAverages(ratings: Seq[Rating]): Map[Int, Double] = {
+    // TODO use groupBy and mapValues etc.
+    val sumByUser = new HashMap[Int, Double]()
+    val countByUser = new HashMap[Int, Int]()
+    for (r <- ratings) {
+      sumByUser.update(r.user, sumByUser.getOrElse(r.user, 0.0) + r.value)
+      countByUser.update(r.user, countByUser.getOrElse(r.user, 0) + 1)
+    }
+    return for ((k, v) <- sumByUser) yield (k, v / countByUser(k))
+  }
+}
+
+class UserAverage(val averages: Map[Int, Double], val globalAverage: GlobalAverage) extends Rater {
+  def this(ratings: Seq[Rating]) = this(Util.userAverages(ratings), new GlobalAverage(ratings))
+  def rate(user: Int, item: Int) = averages.getOrElse(user, globalAverage.average)
 }
 
 
@@ -22,7 +44,7 @@ object Tsv {
     val lines = Source.fromFile(filename).getLines()
     val ratings = for (line <- lines) yield {
       val fields = line.split("\\t")
-      new Rating(new User(fields(0).toInt), new Item(fields(1).toInt), fields(2).toDouble)
+      new Rating(fields(0).toInt, fields(1).toInt, fields(2).toDouble)
     }
     return ratings toList
   }
@@ -31,7 +53,7 @@ object Tsv {
 
 object Eval {
   def rmse(rater: Rater, ratings: Seq[Rating]): Double = {
-    val distances = ratings map (r =>  math.pow(rater.rate(r.user, r.item) - r.value, 2))
+    val distances = ratings map (r => math.pow(rater.rate(r.user, r.item) - r.value, 2))
     val sum = distances.foldLeft(0.0)(_ + _)
     return sum / distances.length
   }
@@ -40,10 +62,22 @@ object Eval {
 
 object Run {
   def main(args: Array[String]) {
+    // read in data
     val train = Tsv.readRatings(args(0))
+    println("training ratings: " + train.size)
     val test = Tsv.readRatings(args(1))
-    val recommender = new GlobalAverage(train)
-    println("#ratings: " + test.size)
-    println("RMSE: " + Eval.rmse(recommender, test))
+    println("test ratings: " + test.size)
+    println()
+
+    // create recommenders
+    val ga = new GlobalAverage(train)
+    val ua = new UserAverage(train)
+
+    // evaluate
+    for (recommender <- Vector(ga, ua)) {
+      println(recommender)
+      println("RMSE: " + Eval.rmse(recommender, test))
+      println()
+    }
   }
 }
