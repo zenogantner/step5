6ec7ca91ea3cb9b9cf4f82875c98c2f1616cde8f
Zeno Gantner <zeno.gantner@gmail.com>
2015-02-26 17:38:12 +0100

    add item average

--- Step5.scala
+++ Step5.scala
@@ -46,22 +46,35 @@ case class GlobalAverage(ratings: Seq[Rating]) extends Rater {
 }
 
 
-// TODO define ItemAverage using minimal code duplication
-case class UserAverage(ratings: Seq[Rating], globalAverage: GlobalAverage) extends Rater {
-  private lazy val averages: Map[Int, Double] = {
-    val sumByUser = mutable.HashMap.empty[Int, Double]
-    val countByUser = mutable.HashMap.empty[Int, Int]
+// TODO do this in a nicer, more functional fashion
+object AvgBy {
+  def apply(ratings: Seq[Rating], selector: Function1[Rating, Int]): Map[Int, Double] = {
+    val sumBy = mutable.HashMap.empty[Int, Double]
+    val countBy = mutable.HashMap.empty[Int, Int]
     for (r <- ratings) {
-      sumByUser(r.user) = sumByUser.getOrElse(r.user, 0.0) + r.value
-      countByUser(r.user) = countByUser.getOrElse(r.user, 0) + 1
+      sumBy(selector(r)) = sumBy.getOrElse(selector(r), 0.0) + r.value
+      countBy(selector(r)) = countBy.getOrElse(selector(r), 0) + 1
     }
-    for ((k, v) <- sumByUser.toMap) yield k -> v / countByUser(k)
+    for ((k, v) <- sumBy.toMap) yield k -> v / countBy(k)
   }
+}
+
+case class UserAverage(ratings: Seq[Rating], globalAverage: GlobalAverage) extends Rater {
+  private lazy val averages: Map[Int, Double] = AvgBy(ratings, _.user)
 
   def rate(user: Int, item: Int) = averages.getOrElse(user, globalAverage.average)
 }
 object UserAverage {
   def apply(ratings: Seq[Rating]): UserAverage = apply(ratings, GlobalAverage(ratings))
+}
+
+case class ItemAverage(ratings: Seq[Rating], globalAverage: GlobalAverage) extends Rater {
+  private lazy val averages: Map[Int, Double] = AvgBy(ratings, _.item)
+
+  def rate(user: Int, item: Int) = averages.getOrElse(item, globalAverage.average)
+}
+object ItemAverage {
+  def apply(ratings: Seq[Rating]): ItemAverage = apply(ratings, GlobalAverage(ratings))
 }
 
 
@@ -77,12 +90,13 @@ object Run extends App {
   // create raters
   val ga = GlobalAverage(train)
   val ua = UserAverage(train)
+  val ia = ItemAverage(train)
 
   val trainer = new MFTrainer()
   val mf = trainer.train(train) // TODO haha, I guess we need to think about naming a bit here
 
   // evaluate
-  for (rater <- Seq(ga, ua, mf)) {
+  for (rater <- Seq(ga, ua, ia, mf)) {
     val rmse = Eval.rmse(rater, test)
     println(s"""${rater.getClass.getSimpleName}
                |RMSE: $rmse
