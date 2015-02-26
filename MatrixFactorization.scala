b63a33219fd117a4be4515cf4ffa3a3ad1851003
Zeno Gantner <zeno.gantner@gmail.com>
2015-02-26 16:11:09 +0100

    simple MF implementation, does not handle unknown users/items yet

--- MatrixFactorization.scala
+++ MatrixFactorization.scala
@@ -1,4 +1,5 @@
 import collection.immutable
+import scala.util.Random
 
 // TODO: Look for a good linear algebra library.
 
@@ -6,6 +7,7 @@ case class MatrixFactorization(val userFactors: Map[Int, Array[Double]],
 			       val itemFactors: Map[Int, Array[Double]]) extends Rater {
   def rate(user: Int, item: Int) = {
     // TODO handle unknown users and items
+    // TODO cut by min and max rating
     1 + dot(userFactors(user), itemFactors(item))
   }
   def dot[T <% Double](as: Iterable[T], bs: Iterable[T]) = {
@@ -17,10 +19,45 @@ case class MatrixFactorization(val userFactors: Map[Int, Array[Double]],
 
 class MFTrainer {
   val k = 5
+  val reg = 0.015
+  val learnRate = 0.01
+  val numIter = 10
+  val rnd = new Random()
+
+  def createFactors(): Array[Double] = List.fill(k)(rnd.nextGaussian * 0.1).toArray
+
+  // TODO move somewhere else; better name?
+  def update(a: Array[Double], b: Array[Double]): Unit = {
+    require(a.size == b.size)
+    for (i <- List.range(0, k - 1)) {
+      a(i) = a(i) + learnRate * b(i)
+    }
+  }
 
   // TODO implement SGD training
-  def train(ratings: Seq[Rating]): MatrixFactorization = new MatrixFactorization(
-    Map[Int, Array[Double]]().withDefaultValue(Array(0, 0, 0, 0, 0)),
-    Map[Int, Array[Double]]().withDefaultValue(Array(0, 0, 0, 0, 0)))
+  def train(ratings: Seq[Rating]): MatrixFactorization = {
+    val users = ratings.map(_.user).distinct
+    val items = ratings.map(_.item).distinct
+    val userFactors: Map[Int, Array[Double]] = users.map(u => u -> createFactors).toMap
+    val itemFactors: Map[Int, Array[Double]] = items.map(i => i -> createFactors).toMap
+
+    val mf = new MatrixFactorization(userFactors, itemFactors)
+
+    // This is numerical computation, please excuse using imperative/non-functional stuff ;-)
+    val shuffledRatings = rnd.shuffle(ratings) // TODO also try indexed access
+    for (i <- 1 to numIter) {
+      // TODO move into its own method
+      for (r <- shuffledRatings) {
+	val uF = userFactors(r.user)
+	val iF = itemFactors(r.item)
+	val err = r.value - mf.rate(r.user, r.item)
+	val userGradient = for (j <- 0 to k-1) yield err * iF(j) - reg * uF(j)
+	update(uF, userGradient.toArray)
+	val itemGradient = for (j <- 0 to k-1) yield err * uF(j) - reg * iF(j)
+	update(iF, itemGradient.toArray)
+      }
+    }
+    mf
+  }
 }
 
